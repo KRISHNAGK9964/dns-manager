154bc751f41294b40a60c72424ce6d8466a6d9ae
KRISHNA K <kkrishna22572@gmail.com>
2024-01-21 15:35:58 +0530

    signin page ✅

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -5,12 +5,17 @@ import { google_mark, apple_mark, undraw_secure, login } from "./../public";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 
+// ------------------------------------------------------------------------------------------------------------ //
+
 interface signinProps {}
 
 interface SigninFormData {
   email: string;
   password: string;
 }
+
+// ------------------------------------------------------------------------------------------------------------- //
+
 const Signin: React.FC<signinProps> = ({}) => {
   const {
     register,
@@ -24,6 +29,7 @@ const Signin: React.FC<signinProps> = ({}) => {
   //onsubmission of form login if no account then signin
   const onSubmit = handleSubmit((formData) => {
     console.log(formData);
+    alert("Credential signin is not supported. please use signin with Google🔧🛠️")
   });
 
   // signin with google provider
@@ -48,25 +54,6 @@ const Signin: React.FC<signinProps> = ({}) => {
     "
       >
         <Image alt="" src={login} className=""></Image>
-        {/* <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[8px] rounded-t-xl h-[172px] max-w-[301px] md:h-[294px] md:max-w-[512px]">
-          <div className="rounded-lg overflow-hidden h-[156px] md:h-[278px] bg-white dark:bg-gray-800">
-            <Image
-              src="https://flowbite.s3.amazonaws.com/docs/device-mockups/laptop-screen.png"
-              className="dark:hidden h-[156px] md:h-[278px] w-full rounded-xl"
-              alt={""}
-              fill={true}
-            />
-            <Image
-              src="https://flowbite.s3.amazonaws.com/docs/device-mockups/laptop-screen-dark.png"
-              className="hidden dark:block h-[156px] md:h-[278px] w-full rounded-lg"
-              alt={""}
-              fill={true}
-            />
-          </div>
-        </div> */}
-        {/* <div className="relative mx-auto bg-gray-900 dark:bg-gray-700 rounded-b-xl rounded-t-sm h-[17px] max-w-[351px] md:h-[21px] md:max-w-[597px]">
-          <div className="absolute left-1/2 top-0 -translate-x-1/2 rounded-b-xl w-[56px] h-[5px] md:w-[96px] md:h-[8px] bg-gray-800"></div>
-        </div> */}
       </div>
       {/* right half */}
       <div className="bg-[#F5F5F5] flex-1 flex justify-center items-center">
