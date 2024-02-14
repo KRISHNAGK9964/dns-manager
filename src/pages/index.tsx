91340469d576b808a974589a11cb0805ce7b3a5e
KRISHNA K <kkrishna22572@gmail.com>
2024-02-14 21:10:15 +0530

    remove comment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,16 +13,6 @@ interface homeProps {}
 // --------------------------------------------------------------------------------------------------- //
 
 const Home: React.FC<homeProps> = ({}) => {
-  const router = useRouter();
-  const { data: session, status } = useSession();
-
-  // whenever the page reloaded or session changed this fucntion will check user.
-  // redirect to signin page if not loggedin
-  // useEffect(() => {
-  //   if (status !== "loading" && !session?.user) {
-  //     router.replace("/signin");
-  //   }
-  // }, [session]);
 
   return (
     <div className="flex flex-col min-h-screen ">
